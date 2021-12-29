cc2577174ba1dca4aa7abe3f17fc5cb4dd0a36c4
David Min Tat Goy <48857727+davidgoy@users.noreply.github.com>
2021-12-29 15:24:04 +0000

    Change folder names

--- Reflect-App/source/reflect/core/src/views/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="footer mt-auto py-3">
-  <div class="container">
-
-    <p class="clearfix text-center text-muted"><small><?php echo $config['siteCopyrightStatement']; ?></small></p>
-
-  </div>
-</footer>
--- Reflect-App/source/reflect/themes/default/src/templates/partials/footer-menu.php
+++
@@ -1,7 +0,0 @@
-<div id="footerMenu" class="mt-2">
-  <p class="text-center">
-    <?php foreach($footerMenu as $menuItem): ?>
-    <a class="mx-2" href="<?php echo $menuItem['url']; ?>"><?php echo $menuItem['title']; ?></a>
-    <?php endforeach; unset($menuItem); ?>
-  </p>
-</div>
